7bd53b1642ce6c1cb561d87c01a5480e9e2bd38c
Toaster <citycodetoaster@gmail.com>
2025-12-07 11:42:16 +0800

    SOME cloudinary in backend setting

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Cloudinary\Cloudinary;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // 確保配置檔案已被讀取
+            $this->mergeConfigFrom(
+                __DIR__ . '/../../config/cloudinary.php', 'cloudinary'
+            );
+            $this->app->singleton('cloudinary', function ($app) {
+                return new Cloudinary([
+                    'cloud' => [
+                        'cloud_name' => config('cloudinary.cloud_name'),
+                        'api_key'    => config('cloudinary.api_key'),
+                        'api_secret' => config('cloudinary.api_secret'),
+                        'secure'     => config('cloudinary.secure', true) // 使用 config 檔案的值
+                    ]
+                ]);
+            });
     }
 
     /**
