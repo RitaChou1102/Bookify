4c68cf73a9ba793f17b1820d80dc87eae6c7d167
Toaster <citycodetoaster@gmail.com>
2025-12-08 02:31:48 +0800

    fix cloudinary import problem in frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,18 +4,12 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import Cloudinary, { CldImage, CldVideo } from '@cloudinary/vue';
-
+import Cloudinary from '@cloudinary/vue';
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
-
 app.use(Cloudinary, {
     cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
 });
-
-app.component('CldImage', CldImage);
-app.component('CldVideo', CldVideo);
-
 app.mount('#app')
